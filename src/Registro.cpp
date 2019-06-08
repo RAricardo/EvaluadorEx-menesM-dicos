b446bb8fbee24afc87f3fabed44c5c4d76bae036
Alex <amonto69@eafit.edu.co>
2019-06-08 16:21:09 -0500

    fixing -ie and -oe

--- src/Registro.cpp
+++ src/Registro.cpp
@@ -4,18 +4,27 @@
 #include <string.h>
 #include <string>
 #include <stdlib.h>
+#include <cstring>
+
+#include <getopt.h>
+
 
 using namespace std;
 
-void init_parameters (char c,int i,string n,int b,int d,int s,int q);
+void init_parameters (int opt,int i,int ie,int oe,string n,int b,int d,int s,int q);
 void stop_parameters(char c,string n);
 void rep_parameters(char c, string n, int i, int m);
 void ctrl_parameters(char c, string n);
 void reg_parameters(char c, string n);
 
-// para init falta [-ie <integer>] [-oe <integer>]
+// ./a.out reg -n memoria prueba.txt prueba2.txt
+
+// ./a.out reg -n memoria - (cosita interactiva)
+
+// ./a.out rep -n memoria (( -i # ó -m # ))
+
 struct Init_struct {
-    int i,b,d,s,q;
+    int i,b,d,s,q,ie,oe;
     string n;
 };
 
@@ -42,9 +51,33 @@ Rep_struct rep_s;
 Ctrl_struct ctrl_s;
 Reg_struct reg_s;
 
+/*  Files  */
+char *infile = NULL;
+FILE *file = stdin;
+FILE *text = stdout;
+char ch[200];
+
+int opt = 0;
+
+static struct option long_options[] = {
+        {"i",   required_argument, 0,  'i' },
+        {"ie",  required_argument, 0,  'r' },
+        {"oe",  required_argument, 0,  't' },
+        {"n",   required_argument, 0,  'n' },
+        {"b",   required_argument, 0,  'b' },
+        {"d",   required_argument, 0,  'd' },
+        {"s",   required_argument, 0,  's' },
+        {"q",   required_argument, 0,  'q' },
+        {0,           0,                 0,  0   }
+};
+
+int long_index =0;
+
 int main( int argc, char  *argv[] )
 {
+
     char c;
+
     char init[] = "init";
     char reg[] = "reg";
     char ctrl[] = "ctrl";
@@ -57,24 +90,30 @@ int main( int argc, char  *argv[] )
     int rep_command = strcmp(argv[1], rep);
     int stop_command = strcmp(argv[1], stop);
 
+    // para init falta [-ie <integer>] [-oe <integer>]
+
     if (init_command == 0) {
 
         printf("Has escogido el comando (%s)", argv[1]);
         cout << endl;
 
         init_s.i=5;
+        init_s.ie=6;
+        init_s.oe=10;
         init_s.b=100;
         init_s.d=100;
         init_s.s=100;
         init_s.q=6;
         init_s.n="evaluator";
 
-        while(( c = getopt( argc, argv, "i:n:b:d:s:q:" )) != -1 ) {
-            init_parameters(c,init_s.i,init_s.n,init_s.b,init_s.d,init_s.s,init_s.q);
+        while ((opt = getopt_long_only(argc, argv,"", long_options, &long_index )) != -1) {
+            init_parameters(opt,init_s.i,init_s.ie,init_s.oe,init_s.n,init_s.b,init_s.d,init_s.s,init_s.q);
         }
 
         cout << "valores finales: " << endl;
         cout << "-i: " << init_s.i << endl;
+        cout << "-ie: " << init_s.ie << endl;
+        cout << "-oe: " << init_s.oe << endl;
         cout << "-n: " << init_s.n << endl;
         cout << "-b: " << init_s.b << endl;
         cout << "-d: " << init_s.d << endl;
@@ -94,6 +133,21 @@ int main( int argc, char  *argv[] )
 
         cout << "valores finales: " << endl;
         cout << "-n: " << reg_s.n << endl;
+
+        if (infile) {
+            file = fopen(infile, "r");
+                if (file == NULL) {
+                    cout << "cannot open input file" << endl;
+                    exit(1);
+                }
+        }
+
+        while (fgets(ch, 200, file) != NULL) {
+            fputs(ch, text);
+        }
+
+        fclose(file);
+        fclose(text);
 
     } else if (ctrl_command == 0) {
 
@@ -151,13 +205,23 @@ int main( int argc, char  *argv[] )
     return 0;
 }
 
-void init_parameters (char c, int i, string n, int b, int d, int s, int q)
+void init_parameters (int opt, int i, int ie, int oe, string n, int b, int d, int s, int q)
 {
-    switch ( c )
+    switch (opt)
     {
         case 'i':
             i=atoi(optarg);
             init_s.i=i;
+            break;
+        //{"ie", optional_argument, 0,  'r' }
+        case 'r':
+            ie=atoi(optarg);
+            init_s.ie=ie;
+            break;
+        //{"oe",    optional_argument, 0,  't' },
+        case 't':
+            oe=atoi(optarg);
+            init_s.oe=oe;
             break;
         case 'n':
             n=(string)optarg;
@@ -179,6 +243,7 @@ void init_parameters (char c, int i, string n, int b, int d, int s, int q)
             q=atoi(optarg);
             init_s.q=q;
             break;
+
         case '?':
             cout<<"Unrecognized option!\n"<<endl;
             break;
@@ -242,6 +307,7 @@ void reg_parameters(char c, string n)
         case 'n':
             n=(string)optarg;
             reg_s.n=n;
+            infile = optarg;
             break;
         case '?':
             cout<<"Unrecognized option!\n"<<endl;
